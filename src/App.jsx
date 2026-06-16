a55942ad4a8927261af930f149aa77198e9fad33
Rajan Himang <rajanhimang@gmail.com>
2026-06-16 23:32:48 +1000

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyle";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+
+
+
+
+const StyleApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyleApp>
+        <Heading as='h1'>The Wild Oasis</Heading>
+        <Heading as='h2'>Check in and out</Heading>
+        <Button onClick={() => alert("check in")}>Check in</Button>
+        <Button onClick={() => alert("check out")}>Check out</Button>
+
+        <Heading as='h3'>Form</Heading>
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
+      </StyleApp>
+    </>
+  );
+}
+
+export default App;
