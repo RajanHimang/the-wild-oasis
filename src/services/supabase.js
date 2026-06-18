61897ad3092baecd73225187b7c4e979e305ef09
Rajan Himang <rajanhimang@gmail.com>
2026-06-18 21:56:29 +1000

    Added features

---
+++ src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://kkgixrkutpwiktnsvcuw.supabase.co";
+const supabaseKey = "sb_publishable_ym5yAj7PK1Pz858BEDSvng_NS9GB1AA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
